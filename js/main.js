458160e33b3509aa87123c7e5f9fb1dc815fa856
Andreas Wagner <a.wagner@tum.de>
2017-06-17 09:47:03 +0200

    removed foundation-icons -> font awesome 4.7.0 changed look of external links

--- js/main.js
+++ js/main.js
@@ -27,12 +27,11 @@ $(document).ready(function () {
 
   $('.btn-mobile-menu').click(function () {
     $('.navigation-wrapper').toggleClass('visible animated bounceInDown')
-    $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn')
+	$('.btn-mobile-menu__icon').toggleClass('fa-list fa-times-circle-o animated fadeIn')
   })
 
   $('.navigation-wrapper .blog-button').click(function () {
     $('.navigation-wrapper').toggleClass('visible')
-    $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn')
   })
 
 })
